45bcdf7565adb9d37cfc1f3694c4d14e5c329110
Valeratal <test4you@yandex.ru>
2023-04-09 21:11:55 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,8 +4,15 @@ import numpy as np
 
 chat_id = 252926140 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(data1: np.ndarray, data2: np.ndarray = None) -> bool:
+    if data2 is None:
+        # Condition 1: two historical samples
+        t, p = stats.ttest_ind(data1, data1, equal_var=True)
+    else:
+        # Conditions 2 and 3: one historical sample and one modified sample
+        t, p = stats.ttest_ind(data1, data2, equal_var=True)
+    
+    alpha = 0.07
+    reject_null = p < alpha
+    
+    return reject_null
